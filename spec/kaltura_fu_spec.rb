582afaf9648348e0ac7e1a3c3df18f4c79deea36
jbcplatter <patricksrobertson@gmail.com>
2010-06-01 11:32:51 -0400

    adding thumbnail basic test

--- spec/kaltura_fu_spec.rb
+++ spec/kaltura_fu_spec.rb
@@ -5,9 +5,12 @@ describe KalturaFu, :type => :helper do
   it "should have the proper javascript include tags" do
     html = helper.include_kaltura_fu
     
-    html.should have_tag("script[src= ?]", "http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js" )
+    html.should have_tag("script[src= ?]", 
+			 "http://ajax.googleapis.com/ajax/libs/swfobject/2.2
+			  /swfobject.js" )
 
-    html.should have_tag("script[src = ?]",%r{/javascripts/kaltura_upload.js\?[0-9]*})    
+    html.should have_tag("script[src = ?]",
+			 %r{/javascripts/kaltura_upload.js\?[0-9]*})    
   end
 
   it "should create a plain thumbnail" do
@@ -15,14 +18,15 @@ describe KalturaFu, :type => :helper do
 
 
     if KalturaFu.config[:thumb_width] && KalturaFu.config[:thumb_height]
-      html.should have_tag("img[src = ?]" ,
-			   "http://www.kaltura.com/p/" + KalturaFu.config[:partner_id +
-			   "/thumbnail/entry_id/12345" + 
-			   "/width/" + KalturaFu.config[:thumb_width] + 
-			   "/height/" + KalturaFu.config[:thumb_height])
+      html.should have_tag("img[src = ?]" , "http://www.kaltura.com/p/" + 
+			   KalturaFu.config[:partner_id] + 
+			   "/thumbnail/entry_id/12345" + "/width/" + 
+			   KalturaFu.config[:thumb_width] + "/height/" + 
+			   KalturaFu.config[:thumb_height])
     else
       html.should have_tag("img[src = ?]", 
-			   "http://www.kaltura.com/p/" + KalturaFu.config[:partner_id +
+			   "http://www.kaltura.com/p/" + 
+			   KalturaFu.config[:partner_id] +
 			   "/thumbnail/entry_id/12345")
     end
   end
