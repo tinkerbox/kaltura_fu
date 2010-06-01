6ae4f2ea474c2bfcc961c888bef0690df5dd3dfd
jbcplatter <patricksrobertson@gmail.com>
2010-06-01 09:52:54 -0400

    making the first test more like a real test

--- spec/kaltura_fu_spec.rb
+++ spec/kaltura_fu_spec.rb
@@ -5,7 +5,9 @@ describe KalturaFu, :type => :helper do
   it "should have a javascript include tag" do
     html = helper.include_kaltura_fu
     
-    html.should have_tag("script[src= ?]", "http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js" )    
+    html.should have_tag("script[src= ?]", "http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js" )
+
+    html.should have_tag("script[src like ?]","/javascripts/kaltura_upload.js?*")    
   end
 
 end
