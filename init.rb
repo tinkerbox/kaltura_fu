c053c08322bdb5847fcdd8716ae39f8fde94a3b7
jbcplatter <patricksrobertson@gmail.com>
2010-06-01 13:41:54 -0400

    refactoring embed_player code

--- init.rb
+++ init.rb
@@ -12,10 +12,11 @@ unless File.exists?(kaltura_yml)
   raise RuntimeError, "Unable to find \"config/kaltura.yml\" file."
 end
 
-#KalturaFu.config = YAML.load_file(kaltura_yml).recursively_symbolize_keys[:development]
+
 KalturaFu.config = YAML.load_file(kaltura_yml).recursively_symbolize_keys[RAILS_ENV.to_sym]
 
 
 unless[:partner_id,:subpartner_id,:administrator_secret].all? {|key| KalturaFu.config.key?(key)}
-  raise RuntimeError, "Kaltura config requires :partner_id, :subpartner_id, and :administrator_secret keys"
+  raise RuntimeError, "Kaltura config requires :partner_id, :subpartner_id,"+
+		      "and :administrator_secret keys"
 end
