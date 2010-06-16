a058d58f11860c2ef8797848b045f4ac4457022e
jbcplatter <patricksrobertson@gmail.com>
2010-06-16 16:39:58 -0400

    added get_video_info

--- lib/kaltura_fu.rb
+++ lib/kaltura_fu.rb
@@ -15,10 +15,10 @@ class Hash
 end
 
 module KalturaFu
-  @config = {}
-  @client = nil
-  @client_configuration = nil
-  @session_key = nil
+  @@config = {}
+  @@client = nil
+  @@client_configuration = nil
+  @@session_key = nil
   mattr_reader :config
   mattr_reader :client
   mattr_reader :session_key
@@ -28,30 +28,49 @@ module KalturaFu
 
   class << self
     def config=(options)
-      @config = options
+      @@config = options
     end
     def create_client_config
-      @client_configuration = Kaltura::KalturaConfiguration.new(@config[:partner_id])
+      @@client_configuration = Kaltura::KalturaConfiguration.new(@@config[:partner_id])
+      @@client_configuration
     end
     
     def create_client
-      if @client_configuration.nil?
+      if @@client_configuration.nil?
         self.create_client_config
       end
-      @client = Kaltura::KalturaClient.new(@client_configuration)
+      @@client = Kaltura::KalturaClient.new(@@client_configuration)
+      @@client
     end
     
     def generate_session_key
-      if @client.nil?
+      if @@client.nil?
         self.create_client
       end
-      @session_key = @client.session_service.start(@config[:administrator_secret],'',KalturaSessionType::ADMIN)
-      @client.ks = @session_key
-      @session_key
+      @@session_key = @@client.session_service.start(@@config[:administrator_secret],'',KalturaSessionType::ADMIN)
+      @@client.ks = @@session_key
+      @@session_key
     end
     
     def clear_session_key!
-      @session_key = nil
+      @@session_key = nil
+    end
+    
+    def get_video_info(video_id)
+      filter = Kaltura::KalturaBaseEntryFilter.new
+      filter.id_in = video_id
+      
+      if @@client.nil?
+        self.generate_session_key
+      end
+      response = @@client.media_service.list(filter)
+      if response.total_count == 1
+        response.objects.first
+      elsif response.total_count == 0
+        raise "ID Not found!"
+      elsif
+        raise "Multiple ID's found!"
+      end    
     end
     
   end
