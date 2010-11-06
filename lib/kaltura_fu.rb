e64d0a95263c2e84ae726f6c45e24910babd179a
patricksrobertson <patricksrobertson@gmail.com>
2010-11-06 09:58:46 -0400

    documentation update for entry.

--- lib/kaltura_fu.rb
+++ lib/kaltura_fu.rb
@@ -1,3 +1,26 @@
+#--
+# Copyright (c) 2010 Velir Studios
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#++
+
 ##
 # The KalturaFu module provides a singleton implementation for Kaltura API interaction.  It stores session and API client information so that they do not need to be reset.
 # @author Patrick Robertson
@@ -11,23 +34,26 @@
 ##
 require 'rubygems'
 require 'kaltura'
+require 'active_support/all'
 
 module KalturaFu
   
   #Initilize the configuration and send the ViewHelpers into ActionView::Base when it's a Rails 3 app.
   require 'kaltura_fu/railtie' if defined?(Rails) && Rails.version.split(".").first == "3"
   
+  autoload :Configuration, 'kaltura_fu/configuration'
+  
   autoload :Video, 'kaltura_fu/video'
   autoload :Category, 'kaltura_fu/category'
   autoload :Report, 'kaltura_fu/report'
-  autoload :Configuration, 'kaltura_fu/configuration'
   autoload :Entry, 'kaltura_fu/entry'
   
   module Entry
-    autoload :Metadata, 'kaltura_fu/entry/metadata'
-    autoload :ClassMethods, 'kaltura_fu/entry/class_methods'
-    autoload :InstanceMethods, 'kaltura_fu/entry/instance_methods'
     
+    autoload :Metadata, 'entry/metadata'
+    autoload :ClassMethods, 'entry/class_methods'
+    autoload :InstanceMethods, 'entry/instance_methods'
+      
     module Metadata
       autoload :ClassMethods, 'kaltura_fu/entry/metadata/class_methods'
       autoload :ClassAndInstanceMethods, 'kaltura_fu/entry/metadata/class_and_instance_methods'
--- lib/kaltura_fu/entry.rb
+++ lib/kaltura_fu/entry.rb
@@ -1,5 +1,69 @@
 module KalturaFu
-  module Entry    
+  
+  ##
+  # The entry module provides a slightly more intuitive interface to the
+  # Kaltura media service.  It determines what retrieval and setting actions 
+  # you can perform based upon the version of the Kaltura-Ruby library using 
+  # reflection.  This allows Kaltura_Fu to be a bit more future proof than the 
+  # Kaltura API client itself!   The tradeoff is that getting/adding/setting 
+  # attributes are defined dynamically.  The current behavior is that the first
+  # call to a dynamic method will then define all of Kaltura's media entries 
+  # methods.  This allows the module to be slightly lighter weight in the event
+  # that it is included in a class but never used, and faster than using method_missing
+  # lookups for 100% of the dynamic methods.  The first call will be slower though,
+  # as it generates numerous other methods.
+  # 
+  # == Usage
+  # The entry module is intended to be used to link Rails Models to Kaltura MediaEntry's.  
+  # However, you should not perform these actions during a web application request.  Doing
+  # so will slow down your request unecessarily.  There is nearly nothing gained from adding a
+  # round trip to your Kaltura server to make an update synchronus.  Instead, this module
+  # should mostly be used in processing a background request from an observer.  
+  #
+  # == Uploading to Kaltura
+  # The entry module provides convienance to uploading directly to your installation of 
+  # Kaltura.  For your web application, there are two Kaltura flash widgets that perform
+  # a much better job of uploading files though.  This functionality has been used in 
+  # production environments that use lecture capture.  A video file is placed in a folder, 
+  # a script picks the file up, and then uploads it into Kaltura.
+  #
+  # The Kaltura API supports uploading media from files, URL's, and also has a batch action.  
+  # The implementation of Kaltura Fu currently ignores the URL and batch methods, instead 
+  # focusing on file uploading.
+  #
+  # @example A basic file upload example:
+  #   media_file = File.new("/path/to/video.mp4")
+  #   upload(media_file,:source => "file")
+  #
+  # @example A file upload with metadata fields populated:
+  #   media_file = File.new("/path/to/video.mp4")
+  #   upload(media_file, :source => "file",
+  #     :name => "My Rad video",
+  #     :description => "I'm capable of such rad things.",
+  #     :tags => "rad,rowdy,video,h.264",
+  #     :categories => "raditude"
+  #   )
+  # == Getting, Setting, and Adding Metadata
+  # The entry module provides an easy mean to retrieve the current state and modify a Kaltura entry.
+  # For metadata fields that act as a list of objects, it also provides an easy way to append values
+  # onto the list.  It uses get_ and set_ instead of the more common Ruby practice of using just the 
+  # attribute and attribute= so that you can include this module in your model without conflict.  Also,
+  # when you are performing actions on the category fields, the module is making sure these are available
+  # in the KMC by calling Kaltura's Category service.
+  # 
+  # @example Retrieving metadata:
+  #   get_name("1_q34aa52a")
+  #   get_categories("1_q34aa52a")
+  #
+  # @example Setting metadata:
+  #   set_name("1_q34aa52a", "waffles")
+  #   set_categories("1_q34aa52a", "HD,h.264,live recording")
+  #
+  # @example appending tags to an existing set:
+  #   add_tags("1_q34aa52a","eductation, lecture capture")
+  ##
+  module Entry
+        
     ##
     # @private
     ##
