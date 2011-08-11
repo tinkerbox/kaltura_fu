e890931be7dc6e32b9f30b6f6215eb13a9a3e03c
patricksrobertson <patricksrobertson@gmail.com>
2011-08-11 12:13:33 -0400

    Fix the configuration to actually work.

--- lib/kaltura_fu/configuration.rb
+++ lib/kaltura_fu/configuration.rb
@@ -16,9 +16,6 @@ module KalturaFu
       :player_conf_id => 'whatever',
       :service_url => 'http://www.kaltura.com'
     }
-    @@client = nil #An insantiated Kaltura::Client class 
-    @@client_configuration = nil #Configuration values for the @@client.
-    @@session_key = nil #The Kaltura ks to use.
     
     attr_accessor :login_email, :login_password, :partner_id, :subpartner_id,
                   :administrator_secret, :user_secret, :thumb_width, :thumb_height,
@@ -32,87 +29,11 @@ module KalturaFu
       @@defaults.each_pair{|k,v| self.send("#{k}=",v)}
     end    
     
-    def self.client
-      @@client ||= nil
-    end
-    
-    def self.client=(value)
-      @@client = value
-    end
-  
-    def self.client_configuration
-      @@client_configuration ||= nil
-    end
-    def self.client_configuration=(value)
-      @@client_configuration = value
-    end
-    def self.session_key
-      @@session_key ||=nil
-    end
-    
-    
-    ##
-    # @private
-    ##
-    def self.create_client_config
-      raise "Missing Partner Identifier" unless @@defaults[:partner_id]
-      @@client_configuration = Kaltura::Configuration.new(@@defaults[:partner_id])
-      @@client_configuration.service_url = @@defaults[:service_url]
-      
-      @@client_configuration
-    end
-
-    ##
-    # @private
-    ##
-    def self.create_client
-      if @@client_configuration.nil?
-        self.create_client_config
-      end
-      @@client = Kaltura::Client.new(@@client_configuration)
-      @@client
-    end
-
-    ##
-    # Generates a Kaltura ks and adds it to the KalturaFu client object.
-    #
-    # @return [String] a Kaltura KS.
-    ## 
-    def self.generate_session_key
-      self.check_for_client_session
-      
-      raise "Missing Administrator Secret" unless @@defaults[:administrator_secret]
-      @@session_key = @@client.session_service.start(@@defaults[:administrator_secret],'',Kaltura::Constants::SessionType::ADMIN)
-      @@client.ks = @@session_key
-    rescue Kaltura::APIError => e
-      puts e.message      
-    end
-    
-    ##
-    # Clears the current Kaltura ks.
-    ##
-    def self.clear_session_key!
-      @@session_key = nil
-    end
-
-    ##
-    # @private
-    ##
-    def self.check_for_client_session
-      if @@client.nil?
-        self.create_client
-        self.generate_session_key
-        true
-      else
-        true
-      end
-    end
-    
   end
   
-  def self.client
-    Configuration.client
-  end
+  @@client = nil #An insantiated Kaltura::Client class 
+  @@client_configuration = nil #Configuration values for the @@client.
+  @@session_key = nil #The Kaltura ks to use.  
   
   def self.config
     Configuration.instance
@@ -122,11 +43,80 @@ module KalturaFu
     yield config
   end
   
-  def self.generate_session_key
-    Configuration.generate_session_key
+  def self.client
+    @@client ||= nil
   end
   
-  def self.check_for_client_session
-    Configuration.check_for_client_session
+  def self.client=(value)
+    @@client = value
   end
+
+  def self.client_configuration
+    @@client_configuration ||= nil
+  end
+  def self.client_configuration=(value)
+    @@client_configuration = value
+  end
+  def self.session_key
+    @@session_key ||=nil
+  end
+  
+  
+  ##
+  # @private
+  ##
+  def self.create_client_config
+    raise "Missing Partner Identifier" unless Configuration.instance.partner_id
+    @@client_configuration = Kaltura::Configuration.new(Configuration.instance.partner_id)
+    @@client_configuration.service_url = Configuration.instance.service_url
+    
+    @@client_configuration
+  end
+
+  ##
+  # @private
+  ##
+  def self.create_client
+    if @@client_configuration.nil?
+      self.create_client_config
+    end
+    @@client = Kaltura::Client.new(@@client_configuration)
+    @@client
+  end
+
+  ##
+  # Generates a Kaltura ks and adds it to the KalturaFu client object.
+  #
+  # @return [String] a Kaltura KS.
+  ## 
+  def self.generate_session_key
+    self.check_for_client_session
+    
+    raise "Missing Administrator Secret" unless Configuration.instance.administrator_secret
+    @@session_key = @@client.session_service.start(Configuration.instance.administrator_secret,'',Kaltura::Constants::SessionType::ADMIN)
+    @@client.ks = @@session_key
+  rescue Kaltura::APIError => e
+    puts e.message      
+  end
+  
+  ##
+  # Clears the current Kaltura ks.
+  ##
+  def self.clear_session_key!
+    @@session_key = nil
+  end
+
+  ##
+  # @private
+  ##
+  def self.check_for_client_session
+    if @@client.nil?
+      self.create_client
+      self.generate_session_key
+      true
+    else
+      true
+    end
+  end  
+  
 end
